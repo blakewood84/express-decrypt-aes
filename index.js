99da38f98f63e418d475708b35f0479f400584a2
Blake Wood Jr <blake.wood.bst@visible.edu>
2021-10-13 11:50:09 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict'
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const cryptoJS = require('crypto-js')
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json()) // To Parse the incoming requests with JSON
+app.use(cors())
+
+app.get('/', async (req, res) => {
+    console.log('$GET -> /')
+    res.send('Hello World! Working great!')
+})
+
+const passphase = '931776028f4b4e0a7fb974226387134c'; // Utilized by both Mobile and BE to decypher key
+
+app.post('/decrypt', async (req, res) => {
+    const body = req.body
+    var bytes  = cryptoJS.AES.decrypt(body.data, passphase);
+    var originalText = bytes.toString(cryptoJS.enc.Utf8);
+
+    console.log(originalText) // original message
+    
+    res.send({
+        data: originText
+    })
+    
+})
+
+app.listen(config.port, () => {
+    console.log(`example app listening at http://localhost:${config.port}`)
+})
